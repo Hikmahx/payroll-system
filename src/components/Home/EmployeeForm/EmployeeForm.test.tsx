632fa2c5e8e1d555b6865f25feba903678295cc4
Hikmahx <hikmayousuph@gmail.com>
2023-06-26 09:52:47 +0100

    testing updated

--- src/components/Home/EmployeeForm/EmployeeForm.test.tsx
+++ src/components/Home/EmployeeForm/EmployeeForm.test.tsx
@@ -98,6 +98,29 @@ describe("Testing Employee Form", () => {
     );
 
     userEvent.click(screen.getByText("Submit"));
+    //
+    await wait();
+
+    const dispatchedActions = store.getActions();
+
+    expect(dispatchedActions[0].meta.arg).toEqual({
+      id: expect.any(Number),
+      name: formData.name,
+      email: formData.email,
+      position: formData.position,
+      cadreLevel: formData.cadreLevel,
+      isAdmin: false,
+      earnings: {
+        basic: formData.earnings.basic,
+        transport: formData.earnings.transport,
+        overtime: formData.earnings.overtime,
+        housing: formData.earnings.housing,
+      },
+      deductions: {
+        tax: formData.deductions.tax,
+        pension: formData.deductions.pension,
+      },
+    });
   });
 
   test("renders 'Update' when 'update' state is true", () => {
@@ -137,4 +160,144 @@ describe("Testing Employee Form", () => {
     const addText = screen.getByText(/Add Employee/i);
     expect(addText).toBeInTheDocument();
   });
+
+  //
+  test("when 'update' state is true", () => {
+    const store: MockStoreEnhanced<any, {}> = mockStore({
+      employees: {
+        update: true,
+      },
+    });
+  
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <EmployeeForm />
+        </Provider>
+      </BrowserRouter>
+    );
+  
+    const update = store.getState().employees.update;
+    expect(update).toBeTruthy();
+  });
+
+
+//
+  test("when form is empty", async () => {
+    const formData = {
+      id: 123456789,
+      name: "John Doe",
+      email: "johndoe@gmail.com",
+      position: "accountant",
+      cadreLevel: "Consultant",
+      isAdmin: false,
+      earnings: {
+        basic: 5000,
+        transport: 1000,
+        overtime: 370,
+        housing: 700,
+      },
+      deductions: {
+        tax: 500,
+        pension: 200,
+      },
+    };
+  
+    const store: MockStoreEnhanced<any, {}> = mockStore({
+      employees: {
+        update: false,
+        employees: [formData],
+        employee: [],
+      },
+    });
+  
+    render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <EmployeeForm />
+        </Provider>
+      </BrowserRouter>
+    );
+  
+    await wait();
+  
+    const update = store.getState().employees.update;
+    expect(update).not.toBeTruthy();
+  
+    
+    expect(
+      (screen.getByPlaceholderText(/Full Name/i) as HTMLInputElement).value
+    ).toBe("");
+    expect(
+      (screen.getByPlaceholderText(/Email/i) as HTMLInputElement).value
+    ).toBe("");
+    expect((screen.getByLabelText("position") as HTMLSelectElement).value).toBe(
+      ""
+    );
+    expect(
+      (screen.getByLabelText("cadreLevel") as HTMLSelectElement).value
+    ).toBe("");
+    expect(
+      (screen.getByPlaceholderText(/Basic Earnings/i) as HTMLInputElement).value
+    ).toBe("");
+    expect(
+      (screen.getByPlaceholderText(/Transport/i) as HTMLInputElement).value
+    ).toBe("");
+    expect(
+      (screen.getByPlaceholderText(/Over time/i) as HTMLInputElement).value
+    ).toBe("");
+    expect(
+      (screen.getByPlaceholderText(/Housing/i) as HTMLInputElement).value
+    ).toBe("");
+    expect(
+      (screen.getByPlaceholderText(/Tax/i) as HTMLInputElement).value
+    ).toBe("");
+    expect(
+      (screen.getByPlaceholderText(/Pension/i) as HTMLInputElement).value
+    ).toBe("");
+  });
+
+  //
+test("updates select option value on change", () => {
+  const employee = {
+    id: "123",
+    name: "John Doe",
+    email: "johndoe@example.com",
+    position: "developer",
+    cadreLevel: "Mid",
+    earnings: {
+      basic: 1000,
+      transport: 200,
+      overtime: 50,
+      housing: 300,
+    },
+    deductions: {
+      tax: 100,
+      pension: 50,
+    },
+  };
+  store = mockStore({
+    employees: {
+      update: true,
+    },
+  });
+  render(
+    <BrowserRouter>
+      <Provider store={store}>
+      <EmployeeForm employee={employee} />
+      </Provider>
+    </BrowserRouter>
+  );
+  const positionSelect = screen.getByLabelText("position");
+
+  expect(positionSelect).toBeInTheDocument();
+  expect(positionSelect.value).toBe(employee.position);
+
+  userEvent.selectOptions(positionSelect, "manager");
+
+  expect(positionSelect.value).toBe("manager");
+});
+
+  
+  
 });
