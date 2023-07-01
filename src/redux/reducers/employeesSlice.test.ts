269f742c4df3bb52d78a070c3f0bafdd3b804fb2
Hikmahx <hikmayousuph@gmail.com>
2023-07-01 16:01:46 +0100

    test updated (file test coverage at 38.02%)

--- src/redux/reducers/employeesSlice.test.ts
+++ src/redux/reducers/employeesSlice.test.ts
@@ -2,13 +2,65 @@ import { configureStore } from "@reduxjs/toolkit";
 // import { act } from "react-dom/test-utils";
 // import { AppDispatch } from "../store";
 import employeesReducer, {
-  addEmployee, EmployeesState,
+  addEmployee,
+  EmployeesState,
+  getEmployees,
   // getEmployees,
   // updateEmployee,
   // updateEmployeeData,
 } from "./employeesSlice";
-import reducer from './employeesSlice'
+import reducer from "./employeesSlice";
 import { Employee } from "./types";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { render } from "../../utils/test-utils";
+
+// We use msw to intercept the network request during the test,
+// and return the response 'John Smith' after 150ms
+// when receiving a get request to the `/api/user` endpoint
+const server = setupServer(
+  rest.get("/api/employees", (req, res, ctx) => {
+    const employeesData = [
+      {
+        id: Date.now(),
+        name: "John Doe",
+        email: "johndoe@gmail.com",
+        position: "accountant",
+        cadreLevel: "Consultant",
+        isAdmin: false,
+        earnings: {
+          basic: 5000,
+          transport: 1000,
+          overtime: 370,
+          housing: 700,
+        },
+        deductions: {
+          tax: 500,
+          pension: 200,
+        },
+      },
+    ];
+    return res(ctx.json(employeesData));
+  })
+);
+
+// Enable API mocking before tests.
+beforeAll(() => server.listen());
+
+// Reset any runtime request handlers we may add during the tests.
+afterEach(() => server.resetHandlers());
+
+// Disable API mocking after the tests are done.
+afterAll(() => server.close());
+
+const initialState = {
+  employees: [],
+  employee: [],
+  error: false,
+  loading: false,
+  errMsg: "",
+  update: false,
+};
 
 describe("employeesSlice", () => {
   let store: ReturnType<typeof configureStore>;
@@ -19,20 +71,92 @@ describe("employeesSlice", () => {
         employees: employeesReducer,
       },
     });
-    
   });
 
-  test('should return the initial state', () => {
+  test("should return the initial state", () => {
     const state = store.getState() as any;
-    expect(state.employees).toEqual(
-      {
-        employees: [],
-        employee: [],
-        error: false,
-        loading: false,
-        errMsg: "" ,
-        update: false,
-      }
-    )
-  })
+    expect(state.employees).toEqual({
+      employees: [],
+      employee: [],
+      error: false,
+      loading: false,
+      errMsg: "",
+      update: false,
+    });
+  });
+
+  test("render getEmployees from slice", async () => {
+    jest.spyOn(getEmployees, "pending");
+    // jest.spyOn(getEmployees, 'fulfilled');
+    const store = configureStore({ reducer: employeesReducer });
+    await store.dispatch(getEmployees());
+
+    expect(await store.getState().employees.length).toBeGreaterThan(0);
+
+    // expect(store.getState().employees).toEqual([formData]);
+    // console.log(store.getState().employees);
+    jest.spyOn(getEmployees, "pending").mockRestore();  
+  });
+
+  test("sets state when getEmployees is pending", () => {
+    const action = { type: getEmployees.pending.type };
+    const state = reducer(initialState, action);
+    expect(state).toEqual({
+      employees: [],
+      employee: [],
+      error: false,
+      loading: true,
+      errMsg: "",
+      update: false,
+    });
+  });
+
+  test("sets state when getEmployees is fulfilled", () => {
+    const formData = {
+      id: Date.now(),
+      name: "John Doe",
+      email: "johndoe@gmail.com",
+      position: "accountant",
+      cadreLevel: "Consultant",
+      isAdmin: false,
+      earnings: {
+        basic: 5000,
+        transport: 1000,
+        overtime: 370,
+        housing: 700,
+      },
+      deductions: {
+        tax: 500,
+        pension: 200,
+      },
+    };
+
+    const action = { type: getEmployees.fulfilled.type, payload: formData };
+    const state = reducer(initialState, action);
+    expect(state).toEqual({
+      employees: formData,
+      employee: [],
+      error: false,
+      loading: false,
+      errMsg: "",
+      update: false,
+    });
+  });
+
+  test("sets state when getEmployees is rejected", () => {
+    const rejectedAction = {
+      type: getEmployees.rejected.type,
+      error: { message: "Error message" }, // Provide an error object with the message property
+    };
+    const state = reducer(initialState, rejectedAction);
+    console.log(state);
+    expect(state).toEqual({
+      employees: [],
+      employee: [],
+      error: true,
+      loading: false,
+      errMsg: "Error message",
+      update: false,
+    });
+  });
 });
